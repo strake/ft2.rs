a67a32cd8927601a3a20a0af21166b5ff351b516
M Farkas-Dyck <strake888@gmail.com>
2018-12-01 02:41:16 -0800

    more lint

--- src/tt_os2.rs
+++ src/tt_os2.rs
@@ -20,87 +20,87 @@ impl TrueTypeOS2Table {
     }
 
     #[inline(always)]
-    pub fn version(&self) -> ffi::FT_UShort {
+    pub fn version(self) -> ffi::FT_UShort {
         unsafe { (*self.raw).version }
     }
 
     #[inline(always)]
-    pub fn avg_char_width(&self) -> ffi::FT_Short {
+    pub fn avg_char_width(self) -> ffi::FT_Short {
         unsafe { (*self.raw).xAvgCharWidth }
     }
 
     #[inline(always)]
-    pub fn us_weight_class(&self) -> ffi::FT_UShort {
+    pub fn us_weight_class(self) -> ffi::FT_UShort {
         unsafe { (*self.raw).usWeightClass }
     }
 
     #[inline(always)]
-    pub fn us_width_class(&self) -> ffi::FT_UShort {
+    pub fn us_width_class(self) -> ffi::FT_UShort {
         unsafe { (*self.raw).usWidthClass }
     }
 
     #[inline(always)]
-    pub fn fs_type(&self) -> ffi::FT_Short {
+    pub fn fs_type(self) -> ffi::FT_Short {
         unsafe { (*self.raw).fsType }
     }
 
     #[inline(always)]
-    pub fn y_subscript_x_size(&self) -> ffi::FT_Short {
+    pub fn y_subscript_x_size(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySubscriptXSize }
     }
 
     #[inline(always)]
-    pub fn y_subscript_y_size(&self) -> ffi::FT_Short {
+    pub fn y_subscript_y_size(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySubscriptYSize }
     }
 
     #[inline(always)]
-    pub fn y_subscript_x_offset(&self) -> ffi::FT_Short {
+    pub fn y_subscript_x_offset(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySubscriptXOffset }
     }
 
     #[inline(always)]
-    pub fn y_subscript_y_offset(&self) -> ffi::FT_Short {
+    pub fn y_subscript_y_offset(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySubscriptYOffset }
     }
 
     #[inline(always)]
-    pub fn y_superscript_x_size(&self) -> ffi::FT_Short {
+    pub fn y_superscript_x_size(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySuperscriptXSize }
     }
 
     #[inline(always)]
-    pub fn y_superscript_y_size(&self) -> ffi::FT_Short {
+    pub fn y_superscript_y_size(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySuperscriptYSize }
     }
 
     #[inline(always)]
-    pub fn y_superscript_x_offset(&self) -> ffi::FT_Short {
+    pub fn y_superscript_x_offset(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySuperscriptXOffset }
     }
 
     #[inline(always)]
-    pub fn y_superscript_y_offset(&self) -> ffi::FT_Short {
+    pub fn y_superscript_y_offset(self) -> ffi::FT_Short {
         unsafe { (*self.raw).ySuperscriptYOffset }
     }
 
     #[inline(always)]
-    pub fn y_strikeout_size(&self) -> ffi::FT_Short {
+    pub fn y_strikeout_size(self) -> ffi::FT_Short {
         unsafe { (*self.raw).yStrikeoutSize }
     }
 
     #[inline(always)]
-    pub fn y_strikeout_position(&self) -> ffi::FT_Short {
+    pub fn y_strikeout_position(self) -> ffi::FT_Short {
         unsafe { (*self.raw).yStrikeoutPosition }
     }
 
     #[inline(always)]
-    pub fn s_family_class(&self) -> ffi::FT_Short {
+    pub fn s_family_class(self) -> ffi::FT_Short {
         unsafe { (*self.raw).sFamilyClass }
     }
 
     #[inline(always)]
-    pub fn x_height(&self) -> ffi::FT_Short {
+    pub fn x_height(self) -> ffi::FT_Short {
         unsafe { (*self.raw).sxHeight }
     }
 }
